c834aa64e52917dc67bc5c6153e244e868d136ee
David Doret <david.doret@me.com>
2014-07-07 18:39:55 +0200

    Test 7

--- Reversal/reversal/GraphApproach/GraphApproach.py
+++ Reversal/reversal/GraphApproach/GraphApproach.py
@@ -148,18 +148,18 @@ class Call(DataObject):
         DataObject.__init__(self, label=label)
         #Initialize pseudo-private members
         self._data_inbound_surface = None
-        self._data_midbound_surface = None
+        self._data_private_surface = None
         self._data_outbound_surface = None
         self._execution_inbound_surface = None
         self._execution_outbound_surface = None
         self._logic = None
         #Call set methods to comply with their logic
         self._set_data_inbound_surface(data_inbound_surface=CallDataInboundSurface())
-        self._set_data_midbound_surface(data_midbound_surface=CallDataMidboundSurface())
+        self._set_data_private_surface(data_private_surface=CallDataPrivateSurface())
         self._set_data_outbound_surface(data_outbound_surface=CallDataOutboundSurface())
         self._set_execution_inbound_surface(execution_inbound_surface=CallExecutionInboundSurface())
         self._set_execution_outbound_surface(execution_outbound_surface=CallExecutionOutboundSurface())
-        self.set_logic(logic)
+        self.set_logic(logic=LogicLibrary.get().get_logic(TypicalLabels.logic_do_nothing))
 
     def get_data_inbound_surface(self):
         return self._data_inbound_surface
@@ -168,12 +168,12 @@ class Call(DataObject):
         data_inbound_surface._set_container(self)
         self._data_inbound_surface = data_inbound_surface
 
-    def get_data_midbound_surface(self):
-        return self._data_midbound_surface
+    def get_data_private_surface(self):
+        return self._data_private_surface
 
-    def _set_data_midbound_surface(self, data_midbound_surface):
-        data_midbound_surface._set_container(self)
-        self._data_midbound_surface = data_midbound_surface
+    def _set_data_private_surface(self, data_private_surface):
+        data_private_surface._set_container(self)
+        self._data_private_surface = data_private_surface
 
     def get_data_outbound_surface(self):
         return self._data_outbound_surface
@@ -201,12 +201,28 @@ class Call(DataObject):
 
     def set_logic(self, logic):
         # Clean inbound ports
-        # Clean midbound ports
+        self.get_data_inbound_surface().reinitialize()
+        # Clean private ports
+        self.get_data_private_surface().reinitialize()
         # Clean outbound ports
+        self.get_data_outbound_surface().reinitialize()
+        # Attach the logic to the call
         self._logic = logic
         # Configure outbound ports
-        # Configure midbound ports
+        for label, data_inbound_port in self.get_logic().get_data_inbound_surface().get_ports().items():
+            data_inbound_port = CallDataInboundPort(container=None, label=label)
+            self.get_data_inbound_surface().set_port(data_inbound_port)
+        # Configure private ports
+        for label, data_inbound_port in self.get_logic().get_data_private_surface().get_ports().items():
+            data_inbound_port = CallDataInboundPort(container=None, label=label)
+            self.get_data_inbound_surface().set_port(data_inbound_port)
         # Configure inbound ports
+        for label, data_inbound_port in self.get_logic().get_data_outbound_surface().get_ports().items():
+            data_outbound_port = CallDataInboundPort(container=None, label=label)
+            self.get_data_inbound_surface().set_port(data_outbound_port)
+
+    def get_logic(self):
+        return self._logic
 
 
 class Logic(DataObject):
@@ -215,7 +231,7 @@ class Logic(DataObject):
     on its parent call.
     A logic component should be able to find ways to implement a
     backward execution on its parent call.
-    A logic component defines a template of inbound, midbound and outbound ports.
+    A logic component defines a template of inbound, private and outbound ports.
     These ports will be copied to the call component where the logic will be executed.
     """
     def __init__(self, container=None, label=None, execute_forward=None, execute_backward=None):
@@ -225,11 +241,11 @@ class Logic(DataObject):
         self.execute_forward = execute_forward
         self.execute_backward = execute_backward
         self._data_inbound_surface = None
-        self._data_midbound_surface = None
+        self._data_private_surface = None
         self._data_outbound_surface = None
         #Call set methods to comply with their logic
         self._set_data_inbound_surface(data_inbound_surface=CallDataInboundSurface())
-        self._set_data_midbound_surface(data_midbound_surface=CallDataMidboundSurface())
+        self._set_data_private_surface(data_private_surface=CallDataPrivateSurface())
         self._set_data_outbound_surface(data_outbound_surface=CallDataOutboundSurface())
 
     def execute_forward(self):
@@ -248,12 +264,12 @@ class Logic(DataObject):
         data_inbound_surface._set_container(self)
         self._data_inbound_surface = data_inbound_surface
 
-    def get_data_midbound_surface(self):
-        return self._data_midbound_surface
+    def get_data_private_surface(self):
+        return self._data_private_surface
 
-    def _set_data_midbound_surface(self, data_midbound_surface):
-        data_midbound_surface._set_container(self)
-        self._data_midbound_surface = data_midbound_surface
+    def _set_data_private_surface(self, data_private_surface):
+        data_private_surface._set_container(self)
+        self._data_private_surface = data_private_surface
 
     def get_data_outbound_surface(self):
         return self._data_outbound_surface
@@ -266,13 +282,38 @@ class Logic(DataObject):
 class LogicLibrary(DataObject):
     """
     """
+
+    _lib = None
+
     def __init__(self):
         self._library = {}
-        add = Logic(container=None, label=TypicalLabels.logic_add, execute_forward=self._add, execute_backward=self._subtract)
-        add.get_data_inbound_surface().set_port(CallDataInboundPort(container=self, XXX))
+        do_nothing = Logic(container=None, \
+                    label=TypicalLabels.logic_do_nothing, \
+                    execute_forward=self._do_nothing, \
+                    execute_backward=self._do_nothing)
+        self.set_logic(do_nothing)
+        add = Logic(container=None, \
+                    label=TypicalLabels.logic_add, \
+                    execute_forward=self._add, \
+                    execute_backward=self._subtract)
+        add.get_data_inbound_surface().set_port(CallDataInboundPort(container=None, \
+                                                                    label=TypicalLabels.inbound_port_1))
+        add.get_data_inbound_surface().set_port(CallDataInboundPort(container=None, \
+                                                                    label=TypicalLabels.inbound_port_2))
+        add.get_data_outbound_surface().set_port(CallDataOutboundPort(container=None, \
+                                                                      label=TypicalLabels.outbound_port_1))
         self.set_logic(add)
         subtract = add.get_reverse(label=TypicalLabels.logic_subtract)
         self.set_logic(subtract)
+
+    @classmethod
+    def get(cls):
+        if LogicLibrary._lib is None:
+            LogicLibrary._lib = LogicLibrary()
+        return(LogicLibrary._lib)
+
+    def _do_nothing(self, call):
+        pass
 
     def _add(self, call):
         x = call.get_data_inbound_surface().get_port(TypicalLabels.inbound_port_1)
@@ -291,14 +332,14 @@ class LogicLibrary(DataObject):
 
     def set_logic(self, logic):
         # Clean all inbound ports
-        # Clean all midbound ports
+        # Clean all private ports
         # Clean all outbound ports
         self._library[logic.get_label()] = logic
-        #TODO: ADD AUTO GENERATION OF PORTS
 
 
 class TypicalLabels(Enum):
     logic_add = "logic_add"
+    logic_do_nothing = "logic_do_nothing"
     logic_subtract = "logic_subtract"
     inbound_port_1 = "a"
     inbound_port_2 = "b"
@@ -311,11 +352,14 @@ class CallDataInboundSurface(DataObject):
     def __init__(self, container=None, label=None):
         #Base classes initialization
         DataObject.__init__(self, container=container, label=label)
-        #Initialize pseudo-private members
         self._data_inbound_ports = {}
 
-    def __repr__(self):
-        return "{0}({1})".format(self.__class__.__name__, self._data_inbound_ports)
+    #def __repr__(self):
+    #    return "{0}({1})".format(self.__class__.__name__, self._data_inbound_ports)
+
+    def reinitialize(self):
+        #Initialize pseudo-private members
+        self._data_inbound_ports = {}
 
     def set_port(self, data_inbound_port):
         #Assign the proper container object
@@ -336,37 +380,50 @@ class CallDataInboundSurface(DataObject):
             return new_port
 
 
-class CallDataMidboundSurface(DataObject):
+    def get_ports(self):
+        return self._data_inbound_ports
+
+
+
+class CallDataPrivateSurface(DataObject):
 
     def __init__(self, container=None, label=None):
         #Base classes initialization
         DataObject.__init__(self, container=container, label=label)
         #Initialize pseudo-private members
-        self._data_midbound_ports = {}
+        self._data_private_ports = {}
 
-    def __repr__(self):
-        return "{0}({1})".format(self.__class__.__name__, self._data_midbound_ports)
+    #def __repr__(self):
+    #    return "{0}({1})".format(self.__class__.__name__, self._data_private_ports)
 
-    def set_port(self, data_midbound_port):
+    def reinitialize(self):
+        #Initialize pseudo-private members
+        self._data_private_ports = {}
+
+    def set_port(self, data_private_port):
         #Assign the proper container object
-        data_midbound_port._set_container(self)
-        if data_midbound_port.get_label() in self._data_midbound_ports:
+        data_private_port._set_container(self)
+        if data_private_port.get_label() in self._data_private_ports:
             # A port with this label exists already,
             # remove it automatically.
-            self._data_midbound_ports.pop(key=data_midbound_port.get_label())
-        self._data_midbound_ports[data_midbound_port.get_label()] = data_midbound_port
+            self._data_private_ports.pop(key=data_private_port.get_label())
+        self._data_private_ports[data_private_port.get_label()] = data_private_port
 
     def get_port(self, label):
-        if label in self._data_midbound_ports:
-            return self._data_midbound_ports(label)
+        if label in self._data_private_ports:
+            return self._data_private_ports(label)
         else:
             # Instead of throwing exceptions, dynamically create a new unlinked port
-            new_port = CallDataMidboundPort(label=label)
+            new_port = CallDataPrivatePort(label=label)
             self.set_port(new_port)
             return new_port
 
+    def get_ports(self):
+        return self._data_private_ports
 
-class CallDataMidboundPort(DataObject):
+
+
+class CallDataPrivatePort(DataObject):
 
     def __init__(self, container=None, label=None, linked_variable=None):
         #Base classes initialization
@@ -391,6 +448,8 @@ class CallDataInboundPort(DataObject):
         #Initialize pseudo-private members
         self._linked_variable = None
         #Call set methods to comply with their logic
+        if linked_variable is None:
+            linked_variable = DataVariable(container=self)
         self.set_linked_variable(linked_variable=linked_variable)
 
     def get_linked_variable(self):
@@ -408,6 +467,10 @@ class CallDataOutboundSurface(DataObject):
         #Initialize pseudo-private members
         self._data_outbound_ports = {}
 
+    def reinitialize(self):
+        #Initialize pseudo-private members
+        self._data_outbound_ports = {}
+
     def set_port(self, data_outbound_port=None):
         #If a pre-configured port is not provided, create one out of the blue.
         if data_outbound_port == None:
@@ -418,7 +481,7 @@ class CallDataOutboundSurface(DataObject):
             # A port with this label exists already,
             # remove it automatically.
             self._data_outbound_ports.pop(key=data_outbound_port.get_label())
-        self._data_outbound_ports[data_outbound_port.label] = data_outbound_port
+        self._data_outbound_ports[data_outbound_port.get_label()] = data_outbound_port
 
     def get_port(self, label=None):
         if label in self._data_outbound_ports:
@@ -429,6 +492,9 @@ class CallDataOutboundSurface(DataObject):
             self.set_port(new_port)
             return new_port
 
+    def get_ports(self):
+        return self._data_outbound_ports
+
 
 class CallDataOutboundPort(DataObject):
 
@@ -438,10 +504,12 @@ class CallDataOutboundPort(DataObject):
         #Initialize pseudo-private members
         self._linked_variable = None
         #Call set methods to comply with their logic
-        self.set__linked_variable(linked_variable=linked_variable)
+        if linked_variable is None:
+            linked_variable = DataVariable(container=self)
+        self.set_linked_variable(linked_variable=linked_variable)
 
-    def __repr__(self):
-        return "{0}({1},LinkedVariable({2}))".format(self.__class__.__name__, super(Labelled, self), self._linked_variable.get_label())
+    #def __repr__(self):
+    #    return "{0}({1},LinkedVariable({2}))".format(self.__class__.__name__, super(Labelled, self), self._linked_variable.get_label())
 
     def get_linked_variable(self):
         return self._linked_variable
@@ -511,9 +579,10 @@ class CallExecutionOutboundSurface(DataObject):
             self.add_port(new_port)
             return new_port
 
+
 class CallExecOutboundPort(DataObject):
 
-    def __init__(self, container,label):
+    def __init__(self, container=None, label=None):
         #Base classes initialization
         DataObject.__init__(self, container=container, label=label)
         #Pseudo-private members
@@ -522,14 +591,20 @@ class CallExecOutboundPort(DataObject):
 
 class DataVariable(DataObject):
 
-    def __init__(self):
-        pass
+    def __init__(self, container=None, label=None):
+        #Base classes initialization
+        DataObject.__init__(self, container=container, label=label)
+        #Pseudo-private members
+        #Public members
 
 
 class DataSurface(DataObject):
 
     def __init__(self):
-        pass
+        #Base classes initialization
+        DataObject.__init__(self, container=container, label=label)
+        #Pseudo-private members
+        #Public members
 
 
 class CallComposite(DataObject):
--- Reversal/reversal/GraphApproach/Test1.py
+++ Reversal/reversal/GraphApproach/Test1.py
@@ -11,6 +11,7 @@ f.get_data_inbound_surface().set_port(CallDataInboundPort(label="coucou"))
 lib = LogicLibrary()
 
 f.set_logic(lib.get_logic(TypicalLabels.logic_add))
+
 #f.get_data_inbound_surface().set_port("a", 2)
 #f.get_data_inbound_surface().set_port("b", 4)
 # f.ex
